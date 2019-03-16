d2dfddd6c14f581648e642a77e2e87b9061e6b04
Janis <janis@customprogrammingsolutions.com>
2019-03-16 18:38:20 +1100

    Empty zip file

--- empty.py
+++ empty.py
@@ -1,3 +1,5 @@
 """The following are the raw bytes contents for various empty files of various file formats"""
 
 wave = b'RIFF....WAVE'
+
+zip_file = b'PK\x05\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
